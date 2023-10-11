148778a4471b79d3523bce8abc20cdee8e609903
Daddy_Figure <stephenonyedikachukwu1@gmail.com>
2023-10-11 16:58:17 -0700

    hard

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+/**
+ * add - Adds two integers and returns the result.
+ * @a: The first integer to add.
+ * @b: The second integer to add.
+ *
+ * Return: The result of adding a and b.
+ */
+int add(int a, int b)
+{
+return a + b;
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototype for _putchar */
+int _putchar(char c);
+
+/* Function prototype for print_alphabet */
+void print_alphabet(void);
+
+/* Function prototype for print_alphabet_x10*/
+void print_alphabet_x10(void);
+
+int print_last_digit(int n);
+
+void print_to_98(int n);
+
+int add(int a, int b);
+
+void times_table(void);
+
+void jack_bauer(void);
+
+int _abs(int n);
+
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+#endif /* MAIN_H */
